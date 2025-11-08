6f544df9330506e755318e232495a71e0b4ca719
unknown <xuantrangk54@gmail.com>
2025-11-08 21:35:42 +0700

    :sparkless: add package local

--- helloworld.go
+++ helloworld.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+	greetingsx2 "ilearngocode/hello/stringlib"
 
-	"github.com/evenskylearngo/greetings/greetings"
+	greetings "github.com/evenskylearngo/greetings/greetings"
 )
 
 func main() {
+	x1 := greetingsx2.RandomFormat()
+	fmt.Println(x1)
 	fmt.Println(greetings.Hello("Go Developer"))
 	fmt.Println(greetings.Goodbye("trang"))
 	fmt.Println(greetings.Plus(1, 2))
---
+++ stringlib/greetingsx2.go
@@ -0,0 +1,33 @@
+package greetingsx2
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
+
+// Hello returns a greeting for the named person.
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return name, errors.New("empty name")
+	}
+	// Create a message using a random format.
+	message := fmt.Sprintf(RandomFormat(), name)
+	return message, nil
+}
+
+// RandomFormat returns one of a set of greeting messages. The returned
+// message is selected at random.
+func RandomFormat() string {
+	// A slice of message formats.
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	// Return a randomly selected message format by specifying
+	// a random index for the slice of formats.
+	return formats[rand.Intn(len(formats))]
+}
